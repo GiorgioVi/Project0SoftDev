631b847dca4edc19bee2b7e21109799187d45735
Xing Tao Shi <xshi@stuy.edu>
2017-10-24 13:59:38 -0400

    stuff

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ my_app.secret_key = os.urandom(32)
 @my_app.route('/', methods=['GET', 'POST'])
 def root():
     if ('user' in session):
-        return render_template('welcome.html', name = session['user'])
+        #return render_template('welcome.html', name = session['user'])
+        return redirect(url_for("home"))
     return render_template('login.html')
 
 @my_app.route('/submitted', methods=['GET','POST'])
@@ -81,6 +82,18 @@ def notInList(newuser, dbobj):
 def logout():
     if ('user' in session):
         session.pop('user')
+    return redirect(url_for("root"))
+
+@my_app.route('/home', methods=['GET','POST'])
+def home():
+    if ('user' in session):
+        return render_template("home.html")
+    return redirect(url_for("root"))
+
+@my_app.route('/createstory', methods=['GET','POST'])
+def home():
+    if ('user' in session):
+        return render_template("create.html")
     return redirect(url_for("root"))
 
 if __name__ == '__main__':
